d74bbc7f63544200aab1846473889db0fa96085a
Maxim <maxim.maltsev@recogizer.de>
2019-05-14 15:30:42 +0200

    experimented with developed functionality

--- examples/developing/main.js
+++ examples/developing/main.js
@@ -7,7 +7,12 @@ let heatOptions = {
   colorGradient: 'Incandescent',
   exporting: true,
   edit: true,
-  keys: true
+  keys: true,
+  defaultData: [
+    {x: 375, y: 84, value: 0.05},
+    {x: 377, y: 84, value: 0.05},
+    {x: 379, y: 88, value: 0.05}
+  ],
 }
 
 Heatimage.heatimage(element, heatOptions)
