8146c03fe3399a1b1640ddc64fc55390dc6b9850
Maxim <maxim.maltsev@recogizer.de>
2019-05-17 13:17:29 +0200

    included dist directory

---
+++ dist/heatimage/palettes.d.ts
@@ -0,0 +1,97 @@
+export declare const gradientPalettes: {
+    'Black Aqua White': {
+        0: string;
+        0.5: string;
+        1: string;
+    };
+    'Blue Red': {
+        0.0: string;
+        1: string;
+    };
+    'Dark Green': {
+        0.1: string;
+        0.2: string;
+        0.4: string;
+        0.5: string;
+        0.6: string;
+        0.7: string;
+        0.8: string;
+        1.0: string;
+    };
+    'Deep Sea': {
+        0.0: string;
+        0.6: string;
+        0.75: string;
+        0.9: string;
+        1.0: string;
+    };
+    'ColorBrewer Blues': {
+        0.0: string;
+        0.2: string;
+        0.4: string;
+        0.5: string;
+        0.6: string;
+        0.8: string;
+        1.0: string;
+    };
+    'Color Spectrum': {
+        0: string;
+        0.25: string;
+        0.5: string;
+        0.75: string;
+        1: string;
+    };
+    'Incandescent': {
+        0: string;
+        0.33: string;
+        0.66: string;
+        1: string;
+    };
+    'Heat': {
+        0.0: string;
+        0.2: string;
+        0.4: string;
+        0.5: string;
+        0.6: string;
+        0.8: string;
+        1.0: string;
+    };
+    'Heated Metal': {
+        0: string;
+        0.4: string;
+        0.6: string;
+        0.8: string;
+        1: string;
+    };
+    'Sunrise': {
+        0: string;
+        0.66: string;
+        1: string;
+    };
+    'Stepped Colors': {
+        0: string;
+        0.25: string;
+        0.26: string;
+        0.5: string;
+        0.51: string;
+        0.75: string;
+        0.76: string;
+        1: string;
+    };
+    'Viridis': {
+        0.0: string;
+        0.2: string;
+        0.4: string;
+        0.5: string;
+        0.6: string;
+        0.8: string;
+        1.0: string;
+    };
+    'Visible Spectrum': {
+        0.00: string;
+        0.25: string;
+        0.50: string;
+        0.75: string;
+        1.00: string;
+    };
+};
